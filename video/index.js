08563ffc4007a0a1eecf154ca46afd2113eb61b7
Admin Quuiz App <128415686+quuizapp@users.noreply.github.com>
2023-03-25 02:16:14 +0700

    Create index.js

---
+++ video/index.js
@@ -0,0 +1,4 @@
+let url = 'https://docs.google.com/spreadsheets/d/19Ep-TK4Pcqz44gHZ8qd3fvxG6nN_xs5liEmW0COqFJY/gviz/tq?';
+const lkjs = document.querySelector('.lkjs');
+const query = encodeURIComponent('SELECT B,C,D,E,F');
+console.log(query);
