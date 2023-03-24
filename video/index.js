33029f724c93155ac67a1a9d0a4e117fbad8bebc
Admin Quuiz App <128415686+quuizapp@users.noreply.github.com>
2023-03-25 02:31:42 +0700

    Update index.js

--- video/index.js
+++ video/index.js
@@ -2,11 +2,3 @@ let url = 'https://docs.google.com/spreadsheets/d/19Ep-TK4Pcqz44gHZ8qd3fvxG6nN_x
 const query = encodeURIComponent('SELECT A,B,C,D');
 console.log(query);
 url = url + '&tq=' + query;
-
-fetch(url)
-.then(res => res.text())
-.then(rep => {
-    const data = JSON.parse(rep.substr(47).slice(0,-2));
-  })
-  console.log(data);
-});
