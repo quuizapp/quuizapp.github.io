7b24a505539705ed7b13f4d3dd9ca6556e854845
Admin Quuiz App <128415686+quuizapp@users.noreply.github.com>
2023-03-25 05:32:15 +0700

    Update index.js

--- video/index.js
+++ video/index.js
@@ -1,16 +1,8 @@
 let url = 'https://docs.google.com/spreadsheets/d/19Ep-TK4Pcqz44gHZ8qd3fvxG6nN_xs5liEmW0COqFJY/gviz/tq?';
 const query = encodeURIComponent('SELECT A,B,C,D');
-const lkjs = document.querySelector('.lkjs');
-const bdsy = document.querySelector('.body');
 console.log(query);
 url = url + '&tq=' + query;
 
-fetch(url)
-.then(res => res.text())
-.then(rep => {
-  const data = JSON.parse(rep.substr(47).slice(0,-2));;
-  const container = document.createElement('pre');
-  bdsy.innerHTML = data;
-  console.log(data.table.rows);
-  bdsy.append(container);
-})
+const myfek = fetch(url);
+const dataText = res.text();
+console.log(dataText);
