a226551700c8c847cbe3724a76e00ad1e1e826da
Admin Quuiz App <128415686+quuizapp@users.noreply.github.com>
2023-03-25 02:48:18 +0700

    Update index.js

--- video/index.js
+++ video/index.js
@@ -7,22 +7,7 @@ url = url + '&tq=' + query;
 fetch(url)
 .then(res => res.text())
 .then(rep => {
-    const data = JSON.parse(rep.substr(47).slice(0,-2));
-    const container = document.createElement('div .demo');
-    container.setAttribute('class', 'row uvh');
-    data.table.rows.forEach((main)=>{
-    lkjs.append(container);
-    console.log(main.c[0]);
-    const imgna = main.c[0];
-    console.log(main.c[1]);
-    const titlena = main.c[1];
-    console.log(main.c[2]);
-    const descna = main.c[2];
-    console.log(main.c[3]);
-    const linkna = main.c[3];
-
-    document.getElementById("demo").innerHTML = data;
-    lkjs.appendChild(container);
-  })
+  const data = JSON.parse(rep.substr(47).slice(0,-2));
   console.log(data);
+  document.getElementById("demo").innerHTML = data;
 })
