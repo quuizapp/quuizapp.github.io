8f3f025cdb3a17c32b450b162382a00ab9089548
Admin Quuiz App <128415686+quuizapp@users.noreply.github.com>
2023-03-25 03:13:34 +0700

    Update index.js

--- video/index.js
+++ video/index.js
@@ -1,6 +1,6 @@
 let url = 'https://docs.google.com/spreadsheets/d/19Ep-TK4Pcqz44gHZ8qd3fvxG6nN_xs5liEmW0COqFJY/gviz/tq?';
 const query = encodeURIComponent('SELECT A,B,C,D');
-const lkjs = document.createElement('body');
+const lkjs = document.querySelector('.lkjs');
 console.log(query);
 url = url + '&tq=' + query;
 
@@ -8,6 +8,7 @@ fetch(url)
 .then(res => res.text())
 .then(rep => {
   const data = JSON.parse(rep.substr(47).slice(0,-2));
+  const container = document.createElement('div');
   data.table.rows.forEach((main)=>{
     lkjs.append(container);
     console.log(main[0]);
